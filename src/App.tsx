fec7a85e5608414590905a49b20110e232178bea
Oliver Racko <oliver-r@outlook.de>
2026-09-18 14:34:58 +0200

    Add units and army selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+
 function App() {
+  const units = [
+    { id: 1, name: 'Zergling', points: 40, category: 'Troop' },
+    { id: 2, name: 'Roach', points: 75, category: 'Troop' },
+    { id: 3, name: 'Queen', points: 150, category: 'Support' },
+  ];
+
+  const [army, setArmy] = useState([]);
+
+  function handleClick(unit) {
+    setArmy([...army, unit]);
+  }
+
+  const totalPoints = army.reduce((total, unit) => {
+    return total + unit.points;
+  }, 0);
+
   return (
     <>
       <div>Army Builder</div>
+      {units.map((unit) => (
+        <div key={unit.id}>
+          <p>{unit.name}</p>
+          <p>{unit.points}</p>
+          <p>{unit.category}</p>
+          <button onClick={() => handleClick(unit)}>+</button>
+        </div>
+      ))}
+
+      <div>
+        <p>My Army</p>
+        {army.map((unit, index) => (
+          <div key={index}>
+            <p>{unit.name}</p>
+          </div>
+        ))}
+        <p>Points: {totalPoints}</p>
+      </div>
     </>
   );
 }
